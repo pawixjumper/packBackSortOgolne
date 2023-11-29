6b9622a25a172a23023a739887814249d61bd3b8
pawixjumper <pawel.czerepak@op.pl>
2023-11-29 08:32:53 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("wpisz liczbe rodzajow pakowanych rzeczy");
+        int size = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("wpisz elementy po kolei a potem wage ok");
+        int[] valueArray = new int[size];
+        int[] weightArray = new int[size];
+        for(int i =0;i<size;i++){
+            valueArray[i] = scanner.nextInt();
+            weightArray[i] = scanner.nextInt();
+        }
+        sortArrays(valueArray,weightArray);
+
+        for(int i = 0;i<size;i++) System.out.print(valueArray[i] + ", ");
+        System.out.println(" ");
+        for(int i = 0;i<size;i++) System.out.print(weightArray[i] + ", ");
+        System.out.println(" ");
+        System.out.println("podaj waga plecaka");
+        int waga = scanner.nextInt();
+        int[] numberUsedProducts = new int[size];
+
+        int value = packBackpack(valueArray,weightArray,waga,numberUsedProducts);
+        System.out.println("wartosc plecaka to : " + value);
+        System.out.println("ilosc kolejnych elementow w plecaku");
+        for(int i =0;i<size;i++) System.out.print(numberUsedProducts[i] +", ");
+
 
     }
 
@@ -15,5 +40,22 @@ public class Main {
             result += numberUserProducts[i] * valueArray[i];
         }
         return result;
+    }
+    public static void sortArrays(int[] valueArray, int[] weightArray){
+        int size = valueArray.length;
+        int valueTemp;
+        int weightTemp;
+        for(int i = 1;i<size;i++){
+            valueTemp = valueArray[i];
+            weightTemp = weightArray[i];
+            for(int j = i -1;j>=0;j--){
+                if(((double) valueArray[j] / weightArray[j])< ((double) valueTemp / weightTemp)){
+                    valueArray[j + 1] = valueArray[j];
+                    weightArray[j + 1] = weightArray[j];
+                    valueArray[j] = valueTemp;
+                    weightArray[j] = weightTemp;
+                }
+            }
+        }
     }
 }
